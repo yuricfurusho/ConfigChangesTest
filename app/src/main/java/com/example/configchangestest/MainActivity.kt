0c4633cc3710137cafed027b1879318d43eb14c1
yurifurusho <yuri>
2019-10-29 15:32:36 -0400

    logging configuration changes

--- app/src/main/java/com/example/configchangestest/MainActivity.kt
+++ app/src/main/java/com/example/configchangestest/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.configchangestest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        Toast.makeText(this, "onConfigurationChanged", Toast.LENGTH_SHORT).show()
     }
 }
